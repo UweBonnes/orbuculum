ddf2a6fbc89ae1bc75129bb374bce1686634a215
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2019-11-19 16:39:58 +0100

    tpiuDecoder: Add timersub define.

--- Src/tpiuDecoder.c
+++ Src/tpiuDecoder.c
@@ -45,6 +45,19 @@
 #define NO_CHANNEL_CHANGE (0xFF)
 #define TIMEOUT (3)
 
+#if !defined(timersub)
+/* This is a copy from GNU C Library (GNU LGPL 2.1), sys/time.h. */
+# define timersub(a, b, result)                                 \
+        do {                                                    \
+                (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;   \
+                (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;\
+                if ((result)->tv_usec < 0) {                    \
+                        --(result)->tv_sec;                     \
+                        (result)->tv_usec += 1000000;           \
+                }                                               \
+        } while (0)
+#endif
+
 // ====================================================================================================
 void TPIUDecoderInit( struct TPIUDecoder *t )
 
